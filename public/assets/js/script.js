2821c51abb7ebdcca511c86db506f904565432af
vijay9987-ui <nmkyvijay666@gmail.com>
2025-08-09 12:53:46 +0530

    22

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -45,38 +45,47 @@ CSS TABLE OF CONTENTS
 	// Preloader area end here ***
 
 	// Mouse cursor area start here ***
-	function mousecursor() {
-		if ($("body")) {
-			const e = document.querySelector(".cursor-inner"),
-				t = document.querySelector(".cursor-outer");
-			let n,
-				i = 0,
-				o = !1;
-			(window.onmousemove = function (s) {
-				o ||
-					(t.style.transform =
-						"translate(" + s.clientX + "px, " + s.clientY + "px)"),
-					(e.style.transform =
-						"translate(" + s.clientX + "px, " + s.clientY + "px)"),
-					(n = s.clientY),
-					(i = s.clientX);
-			}),
-				$("body").on("mouseenter", "a, .cursor-pointer", function () {
-					e.classList.add("cursor-hover"), t.classList.add("cursor-hover");
-				}),
-				$("body").on("mouseleave", "a, .cursor-pointer", function () {
-					($(this).is("a") && $(this).closest(".cursor-pointer").length) ||
-						(e.classList.remove("cursor-hover"),
-						t.classList.remove("cursor-hover"));
-				}),
-				(e.style.visibility = "visible"),
-				(t.style.visibility = "visible");
-		}
-	}
-	$(function () {
-		mousecursor();
-	});
-	// Mouse cursor area end here ***
+function mousecursor() {
+    if (document.querySelector("body")) {
+        const e = document.querySelector(".cursor-inner"),
+              t = document.querySelector(".cursor-outer");
+
+        // If either cursor element doesn't exist, stop
+        if (!e || !t) return;
+
+        let n, i = 0, o = false;
+
+        window.onmousemove = function (s) {
+            if (!o) {
+                t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)";
+            }
+            e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)";
+            n = s.clientY;
+            i = s.clientX;
+        };
+
+        $("body").on("mouseenter", "a, .cursor-pointer", function () {
+            e.classList.add("cursor-hover");
+            t.classList.add("cursor-hover");
+        });
+
+        $("body").on("mouseleave", "a, .cursor-pointer", function () {
+            if (!($(this).is("a") && $(this).closest(".cursor-pointer").length)) {
+                e.classList.remove("cursor-hover");
+                t.classList.remove("cursor-hover");
+            }
+        });
+
+        e.style.visibility = "visible";
+        t.style.visibility = "visible";
+    }
+}
+
+$(function () {
+    mousecursor();
+});
+// Mouse cursor area end here ***
+
 
 	// Header area start here ***
 	// Mobile menu
